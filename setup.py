94d3e79390881e9de765a3844f4026156e7455c9
gezp <zhenpeng.ge@qq.com>
2020-11-23 09:48:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='xacro4sdf',  
-    version='1.2.0',  
+    version='1.2.1',  
     author='Ge Zhenpeng',  
     author_email='zhenpeng.ge@qq.com', 
     url='https://github.com/robomaster-oss/xacro4sdf', 
