510f2e0a2bc084f3549da3039bfe180a971bdcfd
gezp <zhenpeng.ge@qq.com>
2021-05-02 23:21:26 +0800

    add function set_static()

--- test/test_custom_usage.py
+++ test/test_custom_usage.py
@@ -6,4 +6,5 @@ xmacro=XMLMacro()
 xmacro.set_xml_file("model.sdf.xmacro")
 custom_property={"rplidar_a2_h":0.8}
 xmacro.generate(custom_property)
+xmacro.set_static(True)
 xmacro.to_file("custom.sdf",banner_info="test_custom_usage.py")
